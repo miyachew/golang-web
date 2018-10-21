20a1d4385ed29b58875b0995532d9bfe7a9a7a1e
Miya Chew <miya.clt@gmail.com>
2018-10-21 17:57:45 +0800

    update news with template and datatable

--- website.go
+++ website.go
@@ -3,8 +3,8 @@ package main
 import ("fmt"
 				"net/http"
 				"html/template"
-				// "io/ioutil"
-				// "encoding/xml"				
+				"io/ioutil"
+				"encoding/xml"				
 )
 
 type Sitemapindex struct {
@@ -24,7 +24,7 @@ type FormattedNews struct {
 
 type NewsAggPage struct {
     Title string
-    News string
+    News map[string]FormattedNews
 }
 
 func indexHandler(w http.ResponseWriter,r *http.Request){
@@ -33,42 +33,42 @@ func indexHandler(w http.ResponseWriter,r *http.Request){
 	fmt.Fprintf(w,"heeeeello %s","miya")
 }
 
-func newsHandler(w http.ResponseWriter,r *http.Request){
-	// newsMap := getNewsMap("https://www.washingtonpost.com/news-sitemap-index.xml")
-	p := NewsAggPage{ Title : "News from washingtonpost.com", News : "This is a news page"}
-	tmpl, _ := template.ParseFiles("news.html")
-	tmpl.Execute(w,p)
+const newsTemplate string = "news.html"
 
-	// fmt.Fprintf(w, "News from washingtonpost.com")
-	// fmt.Println(p)
+func newsHandler(w http.ResponseWriter,r *http.Request){
+	newsMap := getNewsMap("https://www.washingtonpost.com/news-sitemap-index.xml")
+	p := NewsAggPage{ Title : "News from washingtonpost.com", News : newsMap}
+	tmpl, _ := template.ParseFiles(newsTemplate)
+	tmpl.Execute(w,p)
 }
 
-// func getUrlBytes(url string) []byte{
-// 	res, _ := http.Get(url)
-// 	bytes, _ := ioutil.ReadAll(res.Body)
-// 	res.Body.Close()
-// 	return bytes
-// }
+func getUrlBytes(url string) []byte{
+	res, _ := http.Get(url)
+	bytes, _ := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	return bytes
+}
 
-// func getNewsMap(url string) map[string]FormattedNews{
-// 	MainXmlBytes := getUrlBytes(url)
+func getNewsMap(url string) map[string]FormattedNews{
+	MainXmlBytes := getUrlBytes(url)
 
-// 	var s Sitemapindex
-// 	xml.Unmarshal(MainXmlBytes, &s)
+	var s Sitemapindex
+	xml.Unmarshal(MainXmlBytes, &s)
 
-// 	var news News
-// 	newsMap := make(map[string]FormattedNews)
+	var news News
+	newsMap := make(map[string]FormattedNews)
 
-// 	for _, location := range s.Locations {
-// 		bytes := getUrlBytes(location)
-// 		xml.Unmarshal(bytes,&news)
+	for _, location := range s.Locations {
+		bytes := getUrlBytes(location)
+		xml.Unmarshal(bytes,&news)
 		
-// 		for k, v := range news.Titles {
-// 				newsMap[v] = FormattedNews{ news.Keywords[k],news.Locations[k] }
-// 		}
-// 	}
-// 	return newsMap
-// }
+		for k, v := range news.Titles {
+				newsMap[v] = FormattedNews{ news.Keywords[k],news.Locations[k] }
+		}
+		break
+	}
+	return newsMap
+}
 
 func main(){
 	http.HandleFunc("/",indexHandler)
